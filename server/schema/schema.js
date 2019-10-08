919dc0fc5a4912d9b1dcca02f92a5ba80906b1a7
SImon <jolet.simon@yahoo.com>
2019-10-08 12:06:26 +0200

    First graphql query ok

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,14 @@
 const graphql = require("graphql");
+const lodash = require("lodash");
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+//dummy data
+var books = [
+  { name: "Harry Potter 1", genre: "Enfant", id: "1" },
+  { name: "Harry Potter 2", genre: "Jeunesse", id: "2" },
+  { name: "Harry POtter 7", genre: "Fantastique", id: "3" }
+];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -20,11 +28,13 @@ const RootQuery = new GraphQLObjectType({
     book: {
       // quand quelqu'un recher un book, on arrive ici
       type: BookType, //le type de data que on recherche, défini juste au dessus
-      agrs: { id: { type: GraphQLString } }, //avec la requête book, je vais récupérer un id
+      args: { id: { type: GraphQLString } }, //avec la requête book, je vais récupérer un id
       resolve(parent, args) {
         // à a réception de et ID, on va passer à cette fonction, qui va utiliser l'id et aller chercher le livre lié
         //
         //code to get data from db
+        return lodash.find(books, { id: args.id });
+
         //
       }
     }
