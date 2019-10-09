6c311652557cf66f83a299ef32362eb619705c20
SImon <jolet.simon@yahoo.com>
2019-10-09 10:51:37 +0200

    Leeson 16 except mLab

--- server/app.js
+++ server/app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const mongoose = require("mongoose");
 
 const schema = require("./schema/schema");
 
 const app = express();
+//connect to mLab DataBase (/!\, connection down for Simon, to be resolved)
+mongoose
+  .connect
+  //connection string from the database, à voir sur le site de mlab
+  // /!\ à bien modifier la partie user et la partie password, voir le tuto16, min 4:40
+  ();
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
 
 app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 
